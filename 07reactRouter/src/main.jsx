4bebc27a2b3713a3f0e1ae61d32b6a2e56b6efda
Smit0909 <smitprajapati993@gmail.com>
2024-09-21 18:56:35 +0530

    react router crash course

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import {Home, Contact, About, User, Github} from "./components";
+import { githubInfoLoader } from "./components/Github.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route 
+      loader = {githubInfoLoader}
+      path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
